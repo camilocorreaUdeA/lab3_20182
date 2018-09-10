c6952563488e158405ec8b46c6b37b06d540acc8
camilocorreaUdeA <42076547+camilocorreaUdeA@users.noreply.github.com>
2018-09-10 17:55:35 -0500

    Enunciado practica
    Primera parte de la práctica

---
+++ lab3_code.cpp
@@ -0,0 +1,21 @@
+/* Practica de Laboratorio 3
+*  Informatica 2 - 2018_2
+*  Prof: Juan Camilo Correa Chica
+*  Fecha: Septiembre 10/2018 - Septiembre 17/2018 - Septiembre 24/2018
+*  Plazo máximo para enviar enlace para clonar el repositorio: Septiembre 10/2018 10:00pm
+*  Estudiante1: Nombres y apellidos completos
+*  Estudiante2: Nombres y apellidos completos (Opcional, puede trabajar individualmente)
+*  Fecha del ultimo Commit en GitHub: Agregue la fecha y hora del ultimo Commit (aproximada)
+*
+* 1. Diseñe e implemente una clase llamada "Libro" que tiene los siguientes atributos privados:
+*	Un atributo de tipo entero para el codigo de catalogo del libro
+*	Un atributo de tipo cadena de caracteres para el nombre del libro
+*	Un atributo de tipo char para la materia del libro (L = Literatura, C = Ciencias, G = Geografia, A = Arte, E = Enciclopedia, D = Diccionario)
+*	Un atributo de tipo cadena de caracteres para el nombre del autor
+*	Un atributo tipo bool para indicar si el libro esta disponible o prestado (True = disponible, False = prestado)
+*	
+*	Adicionalmente la clase debe contar con metodos publicos para modificar y obtener el valor de los atributos
+*   La clase debe tener un constructor que permita inicializar todos los atributos de la clase al declarar un objeto (NO es constructor por defecto)
+*	Implemente una funcion amiga que permita buscar un "Libro" por Titulo y por Nombre de Autor
+*	Implemente una clase amiga que permita saber si un "Libro" esta disponible o prestado
+*/
